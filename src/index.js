e1d3d08d0716e9ebc8f8e973c28e0557f9ebfcb4
Yulia <bakhar.yulia@gmail.com>
2019-09-25 09:24:51 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  const arr = expr.split("");
+
+  const openBrackets = arr.filter(item => item === "(");
+  const closeBrackers = arr.filter(item => item === ")");
+
+  if (openBrackets.length !== closeBrackers.length)
+    throw "ExpressionError: Brackets must be paired";
+
+  const result = new Function(`return ${expr}`);
+  if (result() === Infinity) throw "TypeError: Devision by zero.";
+
+  return result();
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator
+};
